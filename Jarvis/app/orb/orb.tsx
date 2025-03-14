ceb5489a6f3d090445de734d9a60f251b2c1f135
konradmh3 <konradm.h.3@gmail.com>
2025-03-13 21:04:12 -0700

    Created connection on server to openAIs realtime api using websockets and recieved pcm16 base64encoded str which i then converted to wave and saved locally

--- Jarvis/app/orb/orb.tsx
+++ Jarvis/app/orb/orb.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
 export function Orb() {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:5000")
+            .then((res) => res.json())
+            .then((data) => setData(data.message))
+            .catch((err) => console.error(err));
+    }, []);
+
+
     return (
-        <div>orb</div>
+        <div>{data || "Loading..."}</div>
     );
 }
---
+++ Server/index.js
@@ -0,0 +1,57 @@
+// ws connection to openAI api
+import WebSocket from "ws";
+import fs from "fs";
+import { spawn } from "child_process";
+
+const url =
+  "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17";
+
+const ws = new WebSocket(url, {
+  headers: {
+    Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+    "OpenAI-Beta": "realtime=v1",
+  },
+});
+
+ws.on("open", function open() {
+  console.log("Connected to server.");
+  const event = {
+    type: "response.create",
+    response: {
+      modalities: ["audio", "text"],
+      instructions: "Give me a haiku about code.",
+    },
+  };
+  ws.send(JSON.stringify(event));
+});
+
+let combinedAudioData = "";
+
+ws.on("message", function incoming(message) {
+  const serverEvent = JSON.parse(message);
+  if (serverEvent.type === "response.audio.delta") {
+    combinedAudioData += serverEvent.delta;
+  }
+  if (serverEvent.type === "response.done") {
+    console.log("Compiled audio response.");
+
+
+    const audioBuffer = Buffer.from(combinedAudioData, "base64");
+    fs.writeFileSync("output.pcm", audioBuffer);
+    // convert to wav
+    const ffmpeg = spawn("ffmpeg", [
+      "-f",
+      "s16le",
+      "-ar",
+      "16k",
+      "-ac",
+      "1",
+      "-i",
+      "output.pcm",
+      "output.wav",
+    ]);
+  }
+  if (serverEvent.type === "response.audio_transcript.done") {
+    console.log("Transcript: ", serverEvent.transcript);
+  }
+});
